8cf5b4e4f15b5b33d9832fa0a3f7b65f13a167a1
Clara Vilela <claravilelajato@gmail.com>
2021-09-24 09:08:56 +0200

    Add first html structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
 import '../styles/App.css';
+import initialData from '../data/clubs.json';
+import { useState } from 'react';
 
 function App() {
+  //datos iniciales json en el estado
+
+  console.log(initialData);
+  const htmlClubsList = initialData.map((oneClub, index) => (
+    <li>
+      Book club
+      <p>Abierto entre semana: Si</p>
+      <p>Abierto el fin de semana:Si</p>
+    </li>
+  ));
+
   return (
     <div>
-      <h1>Hola mundo</h1>
+      <header>
+        <h1>Mis clubs</h1>
+        <ul>{htmlClubsList}</ul>
+      </header>
     </div>
   );
 }
